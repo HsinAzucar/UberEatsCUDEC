62c3789c321371cabec891f3f9e00c3876f37431
HsinAzucar <slerckk@gmail.com>
2026-07-02 21:25:16 -0600

    agregamos botosnes en pedidos y ubicacion

--- js/pedidos.js
+++ js/pedidos.js
@@ -22,4 +22,69 @@ function agregarALista(platillo,id){
     </option>`;
     document.getElementById("ListaPlatillos").innerHTML =contenidoLista;
 }
+
+const btnPedido = document.getElementById("btnPedido");
+
+
+btnPedido.addEventListener("click", () => {
+
+    const platillo = document.getElementById("ListaPlatillos").value;
+    const direccion = document.getElementById("direccion").value;
+
+    db.collection("Pedidos").add({
+        platillo: platillo,
+        direccion: direccion
+        
+    })
+    .then(() => {
+        alert("Pedido realizado correctamente.");
+
+        document.getElementById("direccion").value = "";
+        document.getElementById("ListaPlatillos").selectedIndex = 0;
+
+        M.FormSelect.init(document.querySelectorAll('select'));
+    })
+    .catch((error) => {
+        console.error("Error al guardar el pedido:", error);
+    });
+
+});
+
+
+
 M.AutoInit();
+
+
+
+document.getElementById("btnUbicacion").addEventListener("click", function(){
+    if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(exito, error);
+}
+
+    
+});
+
+function exito(posicion){
+    let latitud = posicion.coords.latitud;
+    let longitud = posicion.coords.longitud;
+    fetch (`https://nominatim.openstreetmap.org/reverse?lat=${latitud}&lon=${longitud}&format=json`,
+        headers:{
+        ' User-Agent': 'UberEatsLeoCUDEC (slerrckk@gmail.com)'
+        }
+    )
+
+
+}
+function error(){
+    alert("no se puede ")
+}
+
+
+
+
+
+
+
+
+
+
