ead240b3429f96b34630a20a9b704c4fef9b2eae
HsinAzucar <slerckk@gmail.com>
2026-07-09 21:17:27 -0600

    agregamos mapa y boton foto

--- js/pedidos.js
+++ js/pedidos.js
@@ -78,8 +78,17 @@ function exito(posicion){
             let ciudad = data.address.city;
             let pais = data.address.country;
             document.getElementById("direccion").value = `${ciudad}, ${pais}`;
+            var map = L.map('mapa').setView([latitud, longitud], 13)
+            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                maxZoom: 19,
+                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetmap</a>'
+
+            }).addTo(map);
+            var marker = L.marker([latitud, longitud]).addTo(map);
+
+            
           })
-        
+        .catch(error => console.error());
 
 }
 function error(error){
